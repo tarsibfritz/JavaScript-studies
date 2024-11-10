f1b6f4b00bb1432f9c9e88d7642a0e23893219a5
Tarsila <tarsilabfritz0305@outlook.com>
2024-11-10 14:45:49 -0300

    RemoveCharacters

---
+++ Practice/07-RemoveCharacters.js
@@ -0,0 +1,7 @@
+// It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry about strings with less than two characters.
+
+function removeChar(str){
+    return str.slice(1, -1)  // creates a substring of str starting from index 1 (the second character) and going up to the second-to-last character (not including the last character). 
+  };
+  
+  console.log(removeChar("Calopsita"))
