0228f04010f675ff9e5745e28ff7e4915159815c
Tarsila <tarsilabfritz0305@outlook.com>
2024-11-10 15:19:00 -0300

    MakeUpperCase

---
+++ Practice/0010-MakeUpperCase.js
@@ -0,0 +1,9 @@
+// Write a function which converts the input string to uppercase.
+
+function makeUpperCase(str) {   
+    let convertWord = str.toUpperCase();
+    
+    return convertWord;
+}
+  
+console.log(makeUpperCase("hello world!"))  
